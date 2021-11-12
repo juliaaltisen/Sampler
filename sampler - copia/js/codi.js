e5a12e1f02aaadd205fb8fd5a5b3f54b3e838aea
juliaaltisen <94177688+juliaaltisen@users.noreply.github.com>
2021-11-12 13:51:11 +0100

    Add files via upload

---
+++ sampler - copia/js/codi.js
@@ -0,0 +1,192 @@
+
+
+
+function playPause() {
+
+    var audio1 = document.getElementById("audio");
+    if (audio1.paused) {
+        audio1.play();
+    } else {
+        audio1.pause();
+    }
+}
+document.getElementById("audio").volume;
+
+document.getElementById("sliderVol").oninput = canviaVolum;
+
+
+function canviaVolum() {
+    document.getElementById("audio").volume =
+        document.getElementById("sliderVol").value;
+}
+
+function playPause2() {
+    var audio2 = document.getElementById("audio2");
+    if (audio2.paused) {
+        audio2.play();
+    
+    } else {
+        audio2.pause();
+
+    }
+}
+document.getElementById("audio2").volume;
+
+document.getElementById("sliderVol2").oninput = canviaVolum2;
+
+
+function canviaVolum2() {
+    document.getElementById("audio2").volume =
+        document.getElementById("sliderVol2").value;
+}
+
+function playPause3() {
+    var audio3 = document.getElementById("audio3");
+    if (audio3.paused) {
+        audio3.play();
+    } else {
+        audio3.pause();
+
+    }
+}
+document.getElementById("audio3").volume;
+
+document.getElementById("sliderVol3").oninput = canviaVolum3;
+
+
+function canviaVolum3() {
+    document.getElementById("audio3").volume =
+        document.getElementById("sliderVol3").value;
+}
+
+function playPause4() {
+    var audio4 = document.getElementById("audio4");
+    if (audio4.paused) {
+        audio4.play();
+
+    } else {
+        audio4.pause();
+
+    }
+}
+document.getElementById("audio4").volume;
+
+document.getElementById("sliderVol4").oninput = canviaVolum4;
+
+
+function canviaVolum4() {
+    document.getElementById("audio4").volume =
+        document.getElementById("sliderVol4").value;
+}
+
+function playPause5() {
+    var audio5 = document.getElementById("audio5");
+    if (audio5.paused) {
+        audio5.play();
+
+    } else {
+        audio5.pause();
+
+    }
+}
+document.getElementById("audio5").volume;
+
+document.getElementById("sliderVol5").oninput = canviaVolum5;
+
+
+function canviaVolum5() {
+    document.getElementById("audio5").volume =
+        document.getElementById("sliderVol5").value;
+}
+function playPause6() {
+    var audio6 = document.getElementById("audio6");
+    if (audio6.paused) {
+        audio6.play();
+
+    } else {
+        audio6.pause();
+
+    }
+}
+document.getElementById("audio6").volume;
+
+document.getElementById("sliderVol6").oninput = canviaVolum6;
+
+
+function canviaVolum6() {
+    document.getElementById("audio6").volume =
+        document.getElementById("sliderVol6").value;
+}
+
+function playPause7() {
+    var audio7 = document.getElementById("audio7");
+    if (audio7.paused) {
+        audio7.play();
+
+    } else {
+        audio7.pause();
+
+    }
+}
+document.getElementById("audio7").volume;
+
+document.getElementById("sliderVol7").oninput = canviaVolum7;
+
+
+function canviaVolum7() {
+    document.getElementById("audio7").volume =
+        document.getElementById("sliderVol7").value;
+}
+function playPause8() {
+    var audio8 = document.getElementById("audio8");
+    if (audio8.paused) {
+        audio8.play();
+
+    } else {
+        audio8.pause();
+
+    }
+}
+document.getElementById("audio7").volume;
+
+document.getElementById("sliderVol8").oninput = canviaVolum8;
+
+
+function canviaVolum8() {
+    document.getElementById("audio8").volume =
+        document.getElementById("sliderVol8").value;
+}
+function playPause9() {
+    var audio9 = document.getElementById("audio9");
+    if (audio9.paused) {
+        audio9.play();
+
+    } else {
+        audio9.pause();
+
+    }
+}
+document.getElementById("audio9").volume;
+
+document.getElementById("sliderVol9").oninput = canviaVolum9;
+
+
+function canviaVolum9() {
+    document.getElementById("audio9").volume =
+        document.getElementById("sliderVol9").value;
+}
+document.getElementById("sliderVolg").oninput = canviaVolumg;
+
+function canviaVolumg() {
+    document.getElementById("audio").volume = document.getElementById("sliderVolg").value;
+    document.getElementById("audio2").volume = document.getElementById("sliderVolg").value;
+    document.getElementById("audio3").volume = document.getElementById("sliderVolg").value;
+    document.getElementById("audio4").volume = document.getElementById("sliderVolg").value;
+    document.getElementById("audio5").volume = document.getElementById("sliderVolg").value;
+    document.getElementById("audio6").volume = document.getElementById("sliderVolg").value;
+    document.getElementById("audio7").volume = document.getElementById("sliderVolg").value;
+    document.getElementById("audio8").volume = document.getElementById("sliderVolg").value;
+    document.getElementById("audio9").volume = document.getElementById("sliderVolg").value;
+   
+
+}
